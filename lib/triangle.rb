bc1d34c2917486c9656d73990e547dc31eae19ad
Laura Radville <laura.radville@gmail.com>
2019-07-31 14:31:56 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,6 +2,14 @@ class Triangle
 
   def initialize(one, two, three)
     @one = o
+    
+    def kind
+      
+    end
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
 
 
 end
