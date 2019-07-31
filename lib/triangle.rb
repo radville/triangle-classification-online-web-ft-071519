6301b55bf86255c42c206312654b2a3742cb2879
Laura Radville <laura.radville@gmail.com>
2019-07-31 14:37:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,9 +18,9 @@ class Triangle
     end
   end
 
-  # class TriangleError < StandardError
-  #   # triangle error code
-  # end
+  class TriangleError < StandardError
+    # triangle error code
+  end
 
 
 end
